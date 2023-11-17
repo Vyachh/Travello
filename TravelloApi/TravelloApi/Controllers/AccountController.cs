38de40c6209577c27c5586a6997f4a6a0c57c4a9
Paco <109136574+Vyachh@users.noreply.github.com>
2023-11-17 10:24:12 +0300

    Dark theme & bugfix

--- TravelloApi/TravelloApi/Controllers/AccountController.cs
+++ TravelloApi/TravelloApi/Controllers/AccountController.cs
@@ -1,7 +1,5 @@
 using AutoMapper;
-using Azure.Core;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
